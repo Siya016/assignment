df7f86e8855153316fe69f6918a365ec3e748d63
Siya <148008409+Siya016@users.noreply.github.com>
2025-08-17 18:22:54 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import BgGradient from "@/components/common/bg-gradient";
 import HeroSection from "@/components/home/hero-section";
-import DemoSection from "@/components/home/demo-section";
+
 import HowItWorksSection from "@/components/home/how-it-works-section";
-import PricingSection from "@/components/home/pricing-section";
+
 import CTASection from "@/components/home/cta-section";
 export default function Home() {
   return (
